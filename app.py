a383c01d6eb72b11bcbb2fab3636cd01b9085064
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2024-11-25 19:19:33 +0530

    second

--- app.py
+++ app.py
@@ -25,191 +25,195 @@ def send_email_hr(gmail_user, app_password, to_email, company_name, hr_name,spec
     smtp_port = 587
 
     # Create the email
-    subject = " Interest in {} Opportunities at {}".format(specific_role,company_name)
-    message_body =f"""
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Interest in AI/ML Opportunities at {company_name}")</title>
-    <style>
-        /* General Reset */
-        * {{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }}
-        body {{
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
-            color: #333;
-            padding: 20px;
-        }}
-        .email-container {{
-            max-width: 650px;
-            margin: auto;
-            background-color: #ffffff;
-            border-radius: 8px;
-            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
-            overflow: hidden;
-            border: 1px solid #e0e0e0;
-        }}
-
-        /* Header */
-        .header {{
-            background:  linear-gradient(135deg, #9b59b6, #e84393);
-            color: #ffffff;
-            padding: 30px;
-            text-align: center;
-            background-image: url('https://ayusharya.me/assets/header.png');
-           background-size: cover;
-           background-repeat: no-repeat;
-           background-position: center;
-           padding: 30px;
-           text-align: center;
-
-        }}
-        .header h1 {{
-            font-size: 24px;
-            font-weight: 700;
-            margin-bottom: 5px;
-        }}
-        .header p {{
-            font-size: 16px;
-            margin-top: 5px;
-        }}
-        /* Content */
-        .content {{
-            padding: 25px 30px;
-            font-size: 16px;
-            line-height: 1.6;
-            color: #555;
-        }}
-        .content p {{
-            margin: 15px 0;
-        }}
-        .highlight {{
-            color: #9b59b6;
-            font-weight: 600;
-        }}
-        .cta-button {{
-            display: inline-block;
-            padding: 12px 24px;
-            margin-top: 20px;
-            background-color: #3498db; /* Solid background for desktop */
-            color: #ffffff;
-            text-decoration: none;
-            font-weight: 600;
-            border-radius: 5px;
-            transition: background-color 0.3s ease;
-        }}
-        .cta-button:hover {{
-            background-color: #2980b9; /* Darker shade on hover for desktop */
-        }}
-
-        /* Responsive styles */
-        @media (max-width: 600px) {{
-            .cta-button {{
-                background-color: #e84393; /* Different color for mobile */
-                padding: 10px 18px;
-                font-size: 15px;
+    subject = " Interest in {} Opportunities at {}".format(specific_role, company_name)
+    message_body = f"""
+          <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title> Interest in AI/ML Opportunities at {company_name}")</title>
+            <style>
+                /* General Reset */
+                * {{
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
                 }}
-            .cta-button:hover {{
-                background-color: #d6336c; /* Darker shade on hover for mobile */}}
+                body {{
+                    font-family: Arial, sans-serif;
+                    background-color: #f5f5f5;
+                    color: #333;
+                    padding: 20px;
+                }}
+                .email-container {{
+                    max-width: 650px;
+                    margin: auto;
+                    background-color: #ffffff;
+                    border-radius: 8px;
+                    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+                    overflow: hidden;
+                    border: 1px solid #e0e0e0;
                 }}
 
+                /* Header */
+                .header {{
+                    background:  linear-gradient(135deg, #9b59b6, #e84393);
+                    color: #ffffff;
+                    padding: 30px;
+                    text-align: center;
+                    background-image: url('https://ayusharya.me/assets/header.png');
+                   background-size: cover;
+                   background-repeat: no-repeat;
+                   background-position: center;
+                   padding: 30px;
+                   text-align: center;
 
-        /* Footer */
-        .footer {{
-            background-color: #e7e7e7;
-            padding: 20px;
-            text-align: center;
-            font-size: 14px;
-            color: #333;
-            border-top: 1px solid #e0e0e0;
-        }}
-        .footer a {{
-            color: black;
-            text-decoration: none;
-            font-weight: 500;
-            margin: 0 10px;
-        }}
-        .footer .icon {{
-            margin: 0 5px;
-            vertical-align: middle;
-        }}
-        .footer img {{
-            width: 20px; /* Size of the icons */
-            height: 20px;
-            vertical-align: middle;
-            margin-right: 5px;
-        }}
+                }}
+                .header h1 {{
+                    font-size: 24px;
+                    font-weight: 700;
+                    margin-bottom: 5px;
+                }}
+                .header p {{
+                    font-size: 16px;
+                    margin-top: 5px;
+                }}
+                /* Content */
+                .content {{
+                    padding: 25px 30px;
+                    font-size: 16px;
+                    line-height: 1.6;
+                    color: #555;
+                }}
+                .content p {{
+                    margin: 15px 0;
+                }}
+                .highlight {{
+                    color: #9b59b6;
+                    font-weight: 600;
+                }}
+                .cta-button {{
+                    display: inline-block;
+                    padding: 12px 24px;
+                    margin-top: 20px;
+                    background-color: #3498db; /* Solid background for desktop */
+                    color: #ffffff;
+                    text-decoration: none;
+                    font-weight: 600;
+                    border-radius: 5px;
+                    transition: background-color 0.3s ease;
+                }}
+                .cta-button:hover {{
+                    background-color: #2980b9; /* Darker shade on hover for desktop */
+                }}
 
-        /* Media Queries for Mobile */
-        @media (max-width: 600px) {{
-            .content {{
-                padding: 20px;
-            }}
-            .header h1 {{
-                font-size: 20px;
-            }}
-            .cta-button {{
-                padding: 10px 18px;
-                font-size: 15px;
-            }}
-            .footer {{
-                padding: 15px;
-            }}
-        }}
-    </style>
-</head>
-<body>
-    <div class="email-container">
-        <!-- Header Section -->
-        <div class="header">
-            <h1> Application for a Role in Your Esteemed Organization</h1>
-            <p>An Enthusiastic Candidate Ready to Make an Impact</p>
+                /* Responsive styles */
+                @media (max-width: 600px) {{
+                    .cta-button {{
+                        background-color: #e84393; /* Different color for mobile */
+                        padding: 10px 18px;
+                        font-size: 15px;
+                        }}
+                    .cta-button:hover {{
+                        background-color: #d6336c; /* Darker shade on hover for mobile */}}
+                        }}
+
+
+                /* Footer */
+                .footer {{
+                    background-color: #e7e7e7;
+                    padding: 20px;
+                    text-align: center;
+                    font-size: 14px;
+                    color: #333;
+                    border-top: 1px solid #e0e0e0;
+                }}
+                .footer a {{
+                    color: black;
+                    text-decoration: none;
+                    font-weight: 500;
+                    margin: 0 10px;
+                }}
+                .footer .icon {{
+                    margin: 0 5px;
+                    vertical-align: middle;
+                }}
+                .footer img {{
+                    width: 20px; /* Size of the icons */
+                    height: 20px;
+                    vertical-align: middle;
+                    margin-right: 5px;
+                }}
+
+                /* Media Queries for Mobile */
+                @media (max-width: 600px) {{
+                    .content {{
+                        padding: 20px;
+                    }}
+                    .header h1 {{
+                        font-size: 20px;
+                    }}
+                    .cta-button {{
+                        padding: 10px 18px;
+                        font-size: 15px;
+                    }}
+                    .footer {{
+                        padding: 15px;
+                    }}
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="email-container">
+                <!-- Header Section -->
+                <div class="header">
+                    <h1> Application for {specific_role} Role at {company_name}</h1>
+                    <p>An Enthusiastic Candidate Ready to Make an Impact</p>
+                </div>
+
+                <!-- Content Section -->
+             <div class="content">
+            <p>Dear <span class="highlight">{hr_name}</span>,</p>
+
+             <p>I hope this email finds you well. My name is Ayush Arya, a final-year B.Tech student specializing in Computer Science and Engineering with a focus on Machine Learning. I am reaching out to express my strong interest in the <span class="highlight"> {specific_role}</span> position at <span class="highlight">{company_name}</span>.</p>
+
+                <p>Having developed projects like a Image Classification  system using Open CV,CNN and a conversational AI tool, I have honed my technical expertise in full-stack development, cloud computing, and problem-solving. My recent internships at Astroverse and IIT BHU have further strengthened my ability to contribute effectively in collaborative and innovative environments.</p>
+
+                <p>I am particularly impressed by your organization's</span> commitment to focus on fostering innovation and delivering impactful solutions that enhance user experiences and drive technological progress, and I would love the opportunity to bring my skills and enthusiasm to your team.</p>
+
+                <p>I have attached my resume for your reference. I would greatly appreciate the chance to discuss how I can contribute to <span class="highlight"> {company_name}'s </span>goals. Please let me know if a convenient time is available for a conversation.</p>
+
+                <p>Thank you for considering my application. I look forward to the possibility of contributing to your team.</p>
+        <p>Warm regards,<br>
+        Ayush Arya<br>
         </div>
 
-        <!-- Content Section -->
-     <div class="content">
-    <p>Dear <span class="highlight">{hr_name}</span>,</p>
-
-     <p>I hope you are doing well. I am Ayush Arya, a final-year B.Tech student in Computer Science and Engineering with a focus on Machine Learning at Graphic Era Hill University. I am excited to express my interest in joining your esteemed organization as a <span class="highlight">{specific_role}</span>.</p>
-
-        <p>Through internships at Astroverse Pvt Ltd and IIT BHU, I developed practical experience in AI, software development, and collaboration. My projects, including Movie Mate and PDF2Chat, reflect my ability to create innovative and impactful solutions.</p>
-
-        <p>I am eager to bring my technical skills, problem-solving approach, and enthusiasm for learning to your team. Please find my resume attached for your reference. I look forward to discussing how I can contribute to your organization.</p>
-
-        <p>Thank you for considering my application.</p>
-
-<p>Warm regards,<br>
-Ayush Arya<br>
-</div>
 
 
 
+                <!-- Footer Section -->
+                <div class="footer">
+                    <p>
+                        <a href="https://ayusharya.me">
+                            <img src="https://img.icons8.com/ios-filled/50/domain.png" class="icon" alt="Website">Website
+                        </a> |
+                        <a href="https://github.com/Ayush01arya">
+                            <img src="https://img.icons8.com/ios-filled/50/000000/github.png" class="icon" alt="Phone">Ayush01arya
+                        </a> |
+                        <a href="https://linkedin.com/in/ayusharya25">
+                            <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" class="icon" alt="LinkedIn">LinkedIn
+                        </a> |
+                        <a href="https://ayusharya.me/assets/resume.pdf">
+                            <img src="https://img.icons8.com/ios-filled/50/resume.png" class="icon" alt="Resume">Resume
+                        </a> 
 
-        <!-- Footer Section -->
-        <div class="footer">
-            <p>
-                <a href="https://ayusharya.me">
-                    <img src="https://img.icons8.com/ios-filled/50/domain.png" class="icon" alt="Website">Website
-                </a> |
-                <a href="tel:+918081133775">
-                    <img src="https://img.icons8.com/ios-filled/50/000000/phone.png" class="icon" alt="Phone">+91-8081133775
-                </a> |
-                <a href="https://linkedin.com/in/ayusharya25">
-                    <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" class="icon" alt="LinkedIn">LinkedIn
-                </a>
-                </a> |
 
-            </p>
-        </div>
-    </div>
-</body>
-</html>"""
+                    </p>
+                </div>
+            </div>
+        </body>
+        </html>"""
     msg = MIMEMultipart()
     msg["From"] = gmail_user
     msg["To"] = to_email
